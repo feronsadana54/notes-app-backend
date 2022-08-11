86a232b36b126064a7265ea5180df4b719c24095
Feron Sadana <feronsadana16@gmail.com>
2022-08-11 10:05:12 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+  addNotesHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  updateNoteHandler,
+  deleteNoteByIdHandler,
+} = require("./handler");
+
+const routes = [
+  {
+    method: "POST",
+    path: "/notes",
+    handler: addNotesHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes",
+    handler: getAllNotesHandler,
+  },
+  {
+    method: "GET",
+    path: `/notes/{id}`,
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: "PUT",
+    path: `/notes/{id}`,
+    handler: updateNoteHandler,
+  },
+  {
+    method: "DELETE",
+    path: `/notes/{id}`,
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
